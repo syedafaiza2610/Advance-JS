e21482335cc3e1957236581631d82dd3067f9ec0
syeda Faiza <syedafaiza2610@gmail.com>
2024-06-25 00:16:36 +0500

    first commit

---
+++ Class1/app.js
@@ -0,0 +1,36 @@
+// document.getElementById('btn').style.background="pink"
+// document.getElementById('btn').innerText="Try me"
+// document.getElementById('head').style.color="red"
+
+
+// console.dir(document.body.childNodes)
+
+// console.dir(document.body.childNodes[1].innerText="Continue")
+
+// let heading = document.getElementById('head');
+// console.dir(heading.innerText)
+
+// let heading = document.getElementsByClassName('para');
+// console.dir(heading)
+
+
+// let  para = document.getElementsByTagName('p');
+// console.dir(para)
+
+// let  para = document.querySelector('p');
+// console.dir(para)
+
+// let  para = document.querySelectorAll('p');
+// console.dir(para)
+
+
+// let heading = document.querySelectorAll('#head');
+// console.dir(heading)
+
+
+// let heading = document.querySelectorAll('.para');
+// console.dir(heading)
+
+
+// let div = document.querySelector('div').children;
+// console.dir(div)
---
+++ Class2/app.js
@@ -0,0 +1,13 @@
+let div = document.querySelector('div')
+console.log(div)
+
+let id = div.getAttribute('id');
+console.log(id)
+
+let update = id.setAttribute('class');
+console.log(id)
+
+
+let name = div.getAttribute('name');
+console.log(name)
+
